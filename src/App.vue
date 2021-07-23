a72712f5b394ff74eef2e6fe916e7784cce488a1
Ryan Wang <hwwangwang@gmail.com>
2021-07-23 14:28:17 +0800

    setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Main />
+</template>
+
+<script setup>
+import Main from "./components/Main.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body html {
+  height: 100%;
+}
+#app {
+  min-height: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="container">
+    <select v-model="currentMonth">
+      <option v-for="month in months" :value="month.value" :key="month.value">
+        {{ month.title }}
+      </option>
+    </select>
+    <canvas ref="canvasRef" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed, watchEffect, watch, shallowRef } from "vue";
+
+function range(start, end) {
+  return Array.apply(null, Array(end - start)).map((_, i) => start + i);
+}
+const canvasRef = ref(null);
+
+const months = ref(
+  range(1, 7).map((i) => ({
+    title: `${i}月`,
+    value: i,
+  }))
+);
+const currentMonth = ref(6);
+
+function adaptData(data) {
+  return [
+    {
+      label: title1,
+      backgroundColor: bg11,
+      borderColor: bg12,
+      data: data[0],
+    },
+    {
+      label: title2,
+      backgroundColor: bg21,
+      borderColor: bg22,
+      data: data[1],
+    },
+  ];
+}
+let bg11 = "#feb2b2";
+let bg12 = "#e53e3e";
+let title1 = "最低价";
+let bg21 = "#d5bcfa";
+let bg22 = "#805ad5";
+let title2 = "最高价";
+const randomArrayInRange = (min, max, n) =>
+  Array.from(
+    { length: n },
+    () => Math.floor(Math.random() * (max - min + 1)) + min
+  );
+
+let datas = [
+  [
+    [
+      90700, 90700, 90700, 90700, 90800, 90800, 90800, 90800, 90800, 90800,
+      90800, 90800, 90900, 91000, 91200,
+    ],
+    [
+      91000, 91000, 91000, 91000, 91000, 91000, 91000, 91000, 91000, 91000,
+      91000, 91000, 91200, 91300, 91500,
+    ],
+  ],
+  [
+    [
+      90800, 90800, 90800, 90900, 90900, 91000, 91000, 91000, 91000, 91000,
+      91000, 91000, 91100, 91200, 91400,
+    ],
+    [
+      91000, 91000, 91000, 91200, 91200, 91300, 91300, 91300, 91300, 91300,
+      91300, 91300, 91400, 91500, 91700,
+    ],
+  ],
+  [
+    [
+      91600, 91700, 91900, 91900, 92000, 92100, 92200, 92400, 92500, 92500,
+      92600, 92700, 92700, 92700, 92800,
+    ],
+    [
+      91900, 92000, 92200, 92200, 92300, 92400, 92500, 92700, 92800, 92800,
+      92900, 93000, 93000, 93000, 93100,
+    ],
+  ],
+  [
+    [
+      91400, 91400, 91600, 91600, 91800, 91800, 91900, 91900, 92100, 92100,
+      92200, 92300, 92400, 92400, 92500,
+    ],
+    [
+      91700, 91700, 91900, 91900, 92100, 92100, 92200, 92200, 92400, 92400,
+      92500, 92600, 92700, 92700, 92800,
+    ],
+  ],
+  [
+    [
+      91000, 91000, 91000, 91000, 91200, 91200, 91300, 91300, 91300, 91300,
+      91300, 91300, 91400, 91400, 91600,
+    ],
+    [
+      91300, 91300, 91400, 91400, 91500, 91500, 91600, 91600, 91600, 91600,
+      91600, 91600, 91700, 91700, 91900,
+    ],
+  ],
+  [
+    [
+      90900, 91000, 91000, 91100, 91100, 91200, 91200, 91200, 91300, 91300,
+      91400, 91500, 91600, 91700, 91700,
+    ],
+    [
+      91200, 91300, 91300, 91400, 91400, 91500, 91500, 91500, 91600, 91600,
+      91700, 91800, 91900, 92000, 92000,
+    ],
+  ],
+];
+datas.reverse();
+
+let myChart = shallowRef(null);
+
+function renderChart() {
+  var ctx = canvasRef.value.getContext("2d");
+  const labels = range(45, 60);
+  const data = {
+    labels: labels,
+    datasets: adaptData(datas[currentMonth.value - 1]),
+  };
+  const config = {
+    type: "line",
+    data,
+    options: {
+      // responsive: true,
+    },
+  };
+
+  myChart.value = new Chart(ctx, config);
+}
+
+function updateChart() {
+  const data = adaptData(datas[currentMonth.value - 1]);
+  const chart = myChart.value;
+  chart.data.datasets[0].data = data[0].data;
+  chart.data.datasets[1].data = data[1].data;
+  chart.update();
+}
+
+onMounted(() => {
+  renderChart();
+});
+watch(currentMonth, () => {
+  updateChart();
+});
+</script>
+
+<style scoped>
+.container {
+  width: 800px;
+}
+</style>
